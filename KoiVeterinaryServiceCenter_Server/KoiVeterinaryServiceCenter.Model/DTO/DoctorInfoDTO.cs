64cd10c88c598edab231a9ea9f96eb67da2e18f2
hoangtuzami <hoangtuzami@gmail.com>
2024-09-23 00:41:54 +0700

    Test

---
+++ KoiVeterinaryServiceCenter_Server/KoiVeterinaryServiceCenter.Model/DTO/DoctorInfoDTO.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KoiVeterinaryServiceCenter.Model.DTO
+{
+    public class DoctorInfoDTO
+    {
+        public string UserId { get; set; }
+        public string Specialization { get; set; }
+        public string Experience { get; set; }
+        public string Degree { get; set; }
+    }
+}
