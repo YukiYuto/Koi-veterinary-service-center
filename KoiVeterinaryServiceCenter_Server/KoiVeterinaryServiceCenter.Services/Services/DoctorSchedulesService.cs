45912b614ca774caad941cbbaf2c450633ba152c
Trần Đức Hiệu <157787787+hoangtuzami@users.noreply.github.com>
2024-10-25 13:50:39 +0700

    Fix ConflictDoctorSchedulesService.cs

--- KoiVeterinaryServiceCenter_Server/KoiVeterinaryServiceCenter.Services/Services/DoctorSchedulesService.cs
+++ KoiVeterinaryServiceCenter_Server/KoiVeterinaryServiceCenter.Services/Services/DoctorSchedulesService.cs
@@ -1,5 +1,6 @@
-﻿using System.Security.Claims;
+using System.Security.Claims;
 using AutoMapper;
+using FirebaseAdmin.Messaging;
 using KoiVeterinaryServiceCenter.DataAccess.IRepository;
 using KoiVeterinaryServiceCenter.Model.DTO;
 using KoiVeterinaryServiceCenter.Models.Domain;
@@ -7,35 +8,32 @@ using KoiVeterinaryServiceCenter.Models.DTO;
 using KoiVeterinaryServiceCenter.Models.DTO.DoctorSchedules;
 using KoiVeterinaryServiceCenter.Services.IServices;
 using Microsoft.IdentityModel.Tokens;
-
 namespace KoiVeterinaryServiceCenter.Services.Services;
-
 public class DoctorSchedulesService : IDoctorSchedulesService
 {
     public readonly IUnitOfWork _unitOfWork;
     public readonly IMapper _mapper;
-
     public DoctorSchedulesService(IUnitOfWork unitOfWork, IMapper mapper)
     {
         _unitOfWork = unitOfWork;
         _mapper = mapper;
     }
-        //Create Schedule for doctor
-        public async Task<ResponseDTO> CreateDoctorSchedule(ClaimsPrincipal User,
-        CreateDoctorSchedulesDTO createDoctorSchedulesDTO)
+
+    //Create Schedule for doctor
+    public async Task<ResponseDTO> CreateDoctorSchedule(ClaimsPrincipal User,
+    CreateDoctorSchedulesDTO createDoctorSchedulesDTO)
+    {
+        try
         {
             //Map DTO to entity
             DoctorSchedules doctorSchedules = new DoctorSchedules()
             {
-                //Map DTO to entity
-                DoctorSchedules doctorSchedules = new DoctorSchedules()
-                {
-                    DoctorId = createDoctorSchedulesDTO.DoctorId,
-                    SchedulesDate = createDoctorSchedulesDTO.SchedulesDate,
-                    CreatedBy = User.Identity.Name,
-                    CreatedTime = DateTime.Now,
-                    Status = 0
-                };
+                DoctorId = createDoctorSchedulesDTO.DoctorId,
+                SchedulesDate = createDoctorSchedulesDTO.SchedulesDate,
+                CreatedBy = User.Identity.Name,
+                CreatedTime = DateTime.Now,
+                Status = 0
+            };
 
             //Add new schedule for doctor
             await _unitOfWork.DoctorSchedulesRepository.AddAsync(doctorSchedules);
@@ -81,12 +79,12 @@ public class DoctorSchedulesService : IDoctorSchedulesService
                     Result = null
                 };
             }
-
             //Use mapping from DoctorSchedules to GetDoctorSchedulesDTO
             try
             {
                 GetDoctorSchedulesDTO doctorSchedulesDTO;
                 doctorSchedulesDTO = _mapper.Map<GetDoctorSchedulesDTO>(doctorSchedule);
+
             }
             //Solve the mapping exception
             catch (AutoMapperMappingException e)
@@ -99,7 +97,6 @@ public class DoctorSchedulesService : IDoctorSchedulesService
                     Result = null
                 };
             }
-
             //If function successfuly, they will return object which is DoctorSchedules and message successfully
             return new ResponseDTO()
             {
@@ -123,14 +120,13 @@ public class DoctorSchedulesService : IDoctorSchedulesService
     }
 
     //Update doctor schedule
-    public async Task<ResponseDTO> UpdateDoctorScheduleById(ClaimsPrincipal User,
-        UpdateDoctorSchedulesDTO updateDoctorSchedulesDTO)
+    //Update doctor schedule
+    public async Task<ResponseDTO> UpdateDoctorScheduleById(ClaimsPrincipal User, UpdateDoctorSchedulesDTO updateDoctorSchedulesDTO)
     {
         try
         {
             //Use ID to find the doctor schedule
-            var updateDoctorSchedules = await _unitOfWork.DoctorSchedulesRepository.GetAsync(x =>
-                x.DoctorSchedulesId == updateDoctorSchedulesDTO.DoctorSchedulesId);
+            var updateDoctorSchedules = await _unitOfWork.DoctorSchedulesRepository.GetAsync(x => x.DoctorSchedulesId == updateDoctorSchedulesDTO.DoctorSchedulesId);
 
             //check doctor schedule is exist or not if this not exist return not exist form
             if (updateDoctorSchedules is null)
@@ -182,8 +178,7 @@ public class DoctorSchedulesService : IDoctorSchedulesService
         try
         {
             //Find doctor schedule from DataBase
-            var doctorScheduleToDelete =
-                await _unitOfWork.DoctorSchedulesRepository.GetAsync(x => x.DoctorSchedulesId == doctorScheduleId);
+            var doctorScheduleToDelete = await _unitOfWork.DoctorSchedulesRepository.GetAsync(x => x.DoctorSchedulesId == doctorScheduleId);
 
             //Resolve error if doctor is null
             if (doctorScheduleToDelete is null)
@@ -231,13 +226,11 @@ public class DoctorSchedulesService : IDoctorSchedulesService
     }
 
     //Get all doctor schedules by filter, sort query and pagination
-    public async Task<ResponseDTO> GetAll(ClaimsPrincipal User, string? filterOn, string? filterQuery, string? sortBy,
-        bool? isAscending, int pageNumber, int pageSize)
+    public async Task<ResponseDTO> GetAll(ClaimsPrincipal User, string? filterOn, string? filterQuery, string? sortBy, bool? isAscending, int pageNumber, int pageSize)
     {
         try
         {
             #region Query Parameters
-
             //Create a new doctor schedules list
             List<DoctorSchedules> doctorSchedules = new List<DoctorSchedules>();
 
@@ -248,49 +241,40 @@ public class DoctorSchedulesService : IDoctorSchedulesService
                 switch (filterOn.Trim().ToLower())
                 {
                     case "doctorname":
-                    {
-                        //get all doctor schedules list with condition which is doctor name must match filterOn
-                        doctorSchedules = _unitOfWork.DoctorSchedulesRepository
-                            .GetAllAsync(includeProperties: "Doctor.ApplicationUser")
-                            .GetAwaiter().GetResult().Where(x =>
-                                x.Doctor.ApplicationUser.FullName.Contains(filterQuery,
-                                    StringComparison.CurrentCultureIgnoreCase)).ToList();
-                        break;
-                    }
+                        {
+                            //get all doctor schedules list with condition which is doctor name must match filterOn
+                            doctorSchedules = _unitOfWork.DoctorSchedulesRepository.GetAllAsync(includeProperties: "Doctor.ApplicationUser")
+                                .GetAwaiter().GetResult().Where(x => x.Doctor.ApplicationUser.FullName.Contains(filterQuery, StringComparison.CurrentCultureIgnoreCase)).ToList();
+                            break;
+                        }
                     //Just for admin
                     case "createdby":
-                    {
-                        //get all doctor schedules list with condition which is created by must match filterOn
-                        doctorSchedules = _unitOfWork.DoctorSchedulesRepository
-                            .GetAllAsync(includeProperties: "Doctor.ApplicationUser")
-                            .GetAwaiter().GetResult().Where(x =>
-                                x.CreatedBy.Contains(filterQuery, StringComparison.CurrentCultureIgnoreCase)).ToList();
-                        break;
-                    }
+                        {
+                            //get all doctor schedules list with condition which is created by must match filterOn
+                            doctorSchedules = _unitOfWork.DoctorSchedulesRepository.GetAllAsync(includeProperties: "Doctor.ApplicationUser")
+                                .GetAwaiter().GetResult().Where(x => x.CreatedBy.Contains(filterQuery, StringComparison.CurrentCultureIgnoreCase)).ToList();
+                            break;
+                        }
                     //Just for admin
                     case "updateby":
-                    {
-                        //get all doctor schedules list with condition which is update by must match filterOn
-                        doctorSchedules = _unitOfWork.DoctorSchedulesRepository
-                            .GetAllAsync(includeProperties: "Doctor.ApplicationUser")
-                            .GetAwaiter().GetResult().Where(x =>
-                                x.UpdatedBy.Contains(filterQuery, StringComparison.CurrentCultureIgnoreCase)).ToList();
-                        break;
-                    }
+                        {
+                            //get all doctor schedules list with condition which is update by must match filterOn
+                            doctorSchedules = _unitOfWork.DoctorSchedulesRepository.GetAllAsync(includeProperties: "Doctor.ApplicationUser")
+                                .GetAwaiter().GetResult().Where(x => x.UpdatedBy.Contains(filterQuery, StringComparison.CurrentCultureIgnoreCase)).ToList();
+                            break;
+                        }
                     default:
                         //get all doctor schedules list without condition
-                        doctorSchedules = _unitOfWork.DoctorSchedulesRepository
-                            .GetAllAsync(includeProperties: "Doctor.ApplicationUser")
-                            .GetAwaiter().GetResult().ToList();
+                        doctorSchedules = _unitOfWork.DoctorSchedulesRepository.GetAllAsync(includeProperties: "Doctor.ApplicationUser")
+                                .GetAwaiter().GetResult().ToList();
                         break;
                 }
             }
             else
             {
                 //get all doctor schedules list without condition
-                doctorSchedules = _unitOfWork.DoctorSchedulesRepository
-                    .GetAllAsync(includeProperties: "Doctor.ApplicationUser")
-                    .GetAwaiter().GetResult().ToList();
+                doctorSchedules = _unitOfWork.DoctorSchedulesRepository.GetAllAsync(includeProperties: "Doctor.ApplicationUser")
+                        .GetAwaiter().GetResult().ToList();
             }
 
             //check sortBy is null or have a value
@@ -301,12 +285,12 @@ public class DoctorSchedulesService : IDoctorSchedulesService
                 {
                     //sort doctor schedules list acsending by name
                     case "name":
-                    {
-                        doctorSchedules = isAscending == true
-                            ? [.. doctorSchedules.OrderBy(name => name.Doctor.ApplicationUser.FullName)]
-                            : [.. doctorSchedules.OrderByDescending(name => name.Doctor.ApplicationUser.FullName)];
-                        break;
-                    }
+                        {
+                            doctorSchedules = isAscending == true
+                                ? [.. doctorSchedules.OrderBy(name => name.Doctor.ApplicationUser.FullName)]
+                                : [.. doctorSchedules.OrderByDescending(name => name.Doctor.ApplicationUser.FullName)];
+                            break;
+                        }
                     //defulat do not anything
                     default:
                         break;
@@ -322,9 +306,7 @@ public class DoctorSchedulesService : IDoctorSchedulesService
                 //list will skip with number above and take with number by page size
                 doctorSchedules = doctorSchedules.Skip(skipResult).Take(pageSize).ToList();
             }
-
             #endregion Query Parameters
-
             //Solve doctor schedules list is null or empty
             if (doctorSchedules.IsNullOrEmpty())
             {
@@ -336,7 +318,6 @@ public class DoctorSchedulesService : IDoctorSchedulesService
                     Result = null
                 };
             }
-
             //Create a new list with object is GetDoctorSchedulesDTO
             List<GetDoctorSchedulesDTO> getDoctorSchedulesDTO = new List<GetDoctorSchedulesDTO>();
             try
@@ -355,7 +336,6 @@ public class DoctorSchedulesService : IDoctorSchedulesService
                     Result = null
                 };
             }
-
             //If function successfuly return doctor schedules list
             return new ResponseDTO()
             {
