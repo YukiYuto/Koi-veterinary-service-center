38942bd78f04c7e53bc71421b024d49dc29c0de0
PhamQuan2003 <bestzed12345ff@gmail.com>
2024-10-27 13:32:02 +0700

    ModifyDB_Doctor

---
+++ KoiVeterinaryServiceCenter_Server/KoiVeterinaryServiceCenter.DataAccess/Migrations/20241027063002_ModifyDB_Doctor.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace KoiVeterinaryServiceCenter.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyDB_Doctor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: new Guid("8586acf6-8437-4648-b4ae-83a7b433cf8d"));
+
+            migrationBuilder.DeleteData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: new Guid("976727cf-2811-44da-a24b-248ae131ba98"));
+
+            migrationBuilder.DeleteData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: new Guid("9d66c406-c412-4fc4-bc86-c06f8c4404a9"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Specialization",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Experience",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Degree",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "GoogleMeetLink",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Position",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "BestZedAndYasuo",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "614be94a-b863-4d72-8eb3-821bbdae3ebb", "AQAAAAIAAYagAAAAEM/C8AExhsgoLxAn3Z9crNvzjbI+fev7rO2vtYgxR7hyWEPbR+lPsKNAaFAFjebNjQ==", "37708464-4024-41e2-9091-56db691c5d06" });
+
+            migrationBuilder.InsertData(
+                table: "EmailTemplates",
+                columns: new[] { "Id", "BodyContent", "CallToAction", "Category", "CreatedBy", "CreatedTime", "FooterContent", "Language", "PersonalizationTags", "PreHeaderText", "RecipientType", "SenderEmail", "SenderName", "Status", "SubjectLine", "TemplateName", "UpdatedBy", "UpdatedTime" },
+                values: new object[,]
+                {
+                    { new Guid("49b1b8af-c881-4370-8c50-df7c3b3f2cf0"), "<p>Thank you for registering your Cursus account. Click here to go back the page</p>", "<a href=\"{{Login}}\">Login now</a>", "Verify", null, null, "<p>Contact us at cursusservicetts@gmail.com</p>", "English", "{FirstName}, {LinkLogin}", "User Account Verified!", "Customer", "cursusservicetts@gmail.com", "Cursus Team", 1, "Cursus Verify Email", "SendVerifyEmail", null, null },
+                    { new Guid("9a5ce45d-b48f-4774-b2e3-354d6562aed9"), "Dear [UserFullName],<br><br>Welcome to Koi Veterinary Service Center! We are thrilled to have you as part of our community dedicated to the care and well-being of your beloved pets.", "<a href=\"{{VerificationLink}}\">Verify Your Email</a>", "Welcome", null, null, "<p>Contact us at koiveterinaryservice@gmail.com</p>", "English", "{FirstName}, {LastName}", "Thank you for signing up!", "Customer", "koiveterinaryservice@gmail.com", "Koi Veterinary Service Center", 1, "Welcome to Koi Veterinary Service Center!", "WelcomeEmail", null, null },
+                    { new Guid("b41d3544-1db5-4ae7-8874-7f3cf937aa39"), "Hi [UserFullName],<br><br>We received a request to reset your password. Click the link below to reset your password.", "https://cursuslms.xyz/sign-in/verify-email?userId=user.Id&token=Uri.EscapeDataString(token)", "Security", null, null, "If you did not request a password reset, please ignore this email.", "English", "[UserFullName], [ResetPasswordLink]", "Reset your password to regain access", "Customer", "cursusservicetts@gmail.com", "Cursus Team", 1, "Reset Your Password", "ForgotPasswordEmail", null, null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: new Guid("49b1b8af-c881-4370-8c50-df7c3b3f2cf0"));
+
+            migrationBuilder.DeleteData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: new Guid("9a5ce45d-b48f-4774-b2e3-354d6562aed9"));
+
+            migrationBuilder.DeleteData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: new Guid("b41d3544-1db5-4ae7-8874-7f3cf937aa39"));
+
+            migrationBuilder.DropColumn(
+                name: "GoogleMeetLink",
+                table: "Doctors");
+
+            migrationBuilder.DropColumn(
+                name: "Position",
+                table: "Doctors");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Specialization",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Experience",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Degree",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "BestZedAndYasuo",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "5065041c-6e04-425d-8735-40afc5bc496b", "AQAAAAIAAYagAAAAECXdJiPBTb6bWXO8uAzTgcaAK7bOsFMgtIUeNWQ2extr0mylMcfAbrWyWkGqGkdhdQ==", "4f9df4b7-1cf4-4269-88c2-f324dab2e08d" });
+
+            migrationBuilder.InsertData(
+                table: "EmailTemplates",
+                columns: new[] { "Id", "BodyContent", "CallToAction", "Category", "CreatedBy", "CreatedTime", "FooterContent", "Language", "PersonalizationTags", "PreHeaderText", "RecipientType", "SenderEmail", "SenderName", "Status", "SubjectLine", "TemplateName", "UpdatedBy", "UpdatedTime" },
+                values: new object[,]
+                {
+                    { new Guid("8586acf6-8437-4648-b4ae-83a7b433cf8d"), "Hi [UserFullName],<br><br>We received a request to reset your password. Click the link below to reset your password.", "https://cursuslms.xyz/sign-in/verify-email?userId=user.Id&token=Uri.EscapeDataString(token)", "Security", null, null, "If you did not request a password reset, please ignore this email.", "English", "[UserFullName], [ResetPasswordLink]", "Reset your password to regain access", "Customer", "cursusservicetts@gmail.com", "Cursus Team", 1, "Reset Your Password", "ForgotPasswordEmail", null, null },
+                    { new Guid("976727cf-2811-44da-a24b-248ae131ba98"), "Dear [UserFullName],<br><br>Welcome to Koi Veterinary Service Center! We are thrilled to have you as part of our community dedicated to the care and well-being of your beloved pets.", "<a href=\"{{VerificationLink}}\">Verify Your Email</a>", "Welcome", null, null, "<p>Contact us at koiveterinaryservice@gmail.com</p>", "English", "{FirstName}, {LastName}", "Thank you for signing up!", "Customer", "koiveterinaryservice@gmail.com", "Koi Veterinary Service Center", 1, "Welcome to Koi Veterinary Service Center!", "WelcomeEmail", null, null },
+                    { new Guid("9d66c406-c412-4fc4-bc86-c06f8c4404a9"), "<p>Thank you for registering your Cursus account. Click here to go back the page</p>", "<a href=\"{{Login}}\">Login now</a>", "Verify", null, null, "<p>Contact us at cursusservicetts@gmail.com</p>", "English", "{FirstName}, {LinkLogin}", "User Account Verified!", "Customer", "cursusservicetts@gmail.com", "Cursus Team", 1, "Cursus Verify Email", "SendVerifyEmail", null, null }
+                });
+        }
+    }
+}
--- KoiVeterinaryServiceCenter_Server/KoiVeterinaryServiceCenter.Models/Domain/Doctor.cs
+++ KoiVeterinaryServiceCenter_Server/KoiVeterinaryServiceCenter.Models/Domain/Doctor.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using KoiVeterinaryServiceCenter.Models.Domain;
 
 namespace KoiVeterinaryServiceCenter.Model.Domain
@@ -14,9 +9,11 @@ namespace KoiVeterinaryServiceCenter.Model.Domain
         [Key] public Guid DoctorId { get; set; }
         public string UserId { get; set; }
         [ForeignKey("UserId")] public virtual ApplicationUser ApplicationUser { get; set; }
-        public string Specialization { get; set; }
-        public string Experience{ get; set; }
-        public string Degree { get; set; }
+        public string? Specialization { get; set; }
+        public string? Experience{ get; set; }
+        public string? Degree { get; set; }
+        public string? Position { get; set; }
+        public string? GoogleMeetLink { get; set; }
 
         [NotMapped]public virtual ICollection<DoctorServices> DoctorServices { get; set; }
     }
