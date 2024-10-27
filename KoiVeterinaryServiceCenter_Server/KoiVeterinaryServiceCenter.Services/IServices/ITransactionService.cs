2b28aac95840ba14892fdb416d45bde300e4c8e0
hoangtuzami <hoangtuzami@gmail.com>
2024-10-27 14:11:44 +0700

    Create interface transaction and transaction service

---
+++ KoiVeterinaryServiceCenter_Server/KoiVeterinaryServiceCenter.Services/IServices/ITransactionService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using KoiVeterinaryServiceCenter.Models.DTO;
+
+namespace KoiVeterinaryServiceCenter.Services.IServices
+{
+    public interface ITransactionService
+    {
+        Task<ResponseDTO> GetAll();
+        Task<ResponseDTO> GetById(Guid transactionId);
+        void Update (Transaction transaction); 
+    }
+}
---
+++ KoiVeterinaryServiceCenter_Server/KoiVeterinaryServiceCenter.Services/Services/TransactionService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using KoiVeterinaryServiceCenter.DataAccess.IRepository;
+using KoiVeterinaryServiceCenter.Models.DTO;
+using KoiVeterinaryServiceCenter.Services.IServices;
+using StackExchange.Redis;
+
+namespace KoiVeterinaryServiceCenter.Services.Services
+{
+    public class TransactionService : ITransactionService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        
+        public TransactionService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task<ResponseDTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO> GetById(Guid transactionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Transaction transaction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
