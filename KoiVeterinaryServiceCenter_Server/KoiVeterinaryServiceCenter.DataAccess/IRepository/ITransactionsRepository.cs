30008f75228b484410552c5872fcf483d081bae1
hoangtuzami <hoangtuzami@gmail.com>
2024-10-28 16:32:06 +0700

    Modify get transaction by id that will have more detail

--- KoiVeterinaryServiceCenter_Server/KoiVeterinaryServiceCenter.DataAccess/IRepository/ITransactionsRepository.cs
+++ KoiVeterinaryServiceCenter_Server/KoiVeterinaryServiceCenter.DataAccess/IRepository/ITransactionsRepository.cs
@@ -5,5 +5,6 @@ namespace KoiVeterinaryServiceCenter.DataAccess.IRepository
     public interface ITransactionsRepository : IRepository<Transaction>
     {
         Task<Transaction> GetTransactionById(Guid transactionId);
+        Task<Transaction> GetTransactionFullInfoById(Guid transactionId);
     }
 }
