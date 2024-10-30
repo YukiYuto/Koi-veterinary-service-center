458ae6118b408be9a332c3a567e5e2b5ae600454
PhamQuan2003 <bestzed12345ff@gmail.com>
2024-10-30 10:08:09 +0700

    ModifyDB_PetAppointmentService

---
+++ KoiVeterinaryServiceCenter_Server/KoiVeterinaryServiceCenter.DataAccess/Migrations/20241030030624_ModifyDB_PetServiceAppointment.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace KoiVeterinaryServiceCenter.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyDB_PetServiceAppointment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: new Guid("1c7b5b31-cf47-447b-aeea-bbed2d5ababa"));
+
+            migrationBuilder.DeleteData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: new Guid("30610ebb-7620-4ab7-b3af-a5642a67691d"));
+
+            migrationBuilder.DeleteData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: new Guid("488e6b78-b7bb-43e1-8678-b70e271ab2b5"));
+
+            migrationBuilder.CreateTable(
+                name: "AppointmentPets",
+                columns: table => new
+                {
+                    AppointmentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PetId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppointmentPets", x => new { x.AppointmentId, x.PetId });
+                    table.ForeignKey(
+                        name: "FK_AppointmentPets_Appointments_AppointmentId",
+                        column: x => x.AppointmentId,
+                        principalTable: "Appointments",
+                        principalColumn: "AppointmentId");
+                    table.ForeignKey(
+                        name: "FK_AppointmentPets_Pets_PetId",
+                        column: x => x.PetId,
+                        principalTable: "Pets",
+                        principalColumn: "PetId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PetServices",
+                columns: table => new
+                {
+                    PetId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ServiceId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PetServices", x => new { x.PetId, x.ServiceId });
+                    table.ForeignKey(
+                        name: "FK_PetServices_Pets_PetId",
+                        column: x => x.PetId,
+                        principalTable: "Pets",
+                        principalColumn: "PetId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PetServices_Services_ServiceId",
+                        column: x => x.ServiceId,
+                        principalTable: "Services",
+                        principalColumn: "ServiceId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "BestZedAndYasuo",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "32e2b56e-456e-4a7e-9f5c-857a801529c1", "AQAAAAIAAYagAAAAEPdOEK5MA0BzRGj16Q6JUJex8JnZJuIsT6IzTXWmuxu7zRQ/NfEMIv779cT60RXfyg==", "df96ba3d-d1ed-41b7-ac4e-0d4195f0caeb" });
+
+            migrationBuilder.InsertData(
+                table: "EmailTemplates",
+                columns: new[] { "Id", "BodyContent", "CallToAction", "Category", "CreatedBy", "CreatedTime", "FooterContent", "Language", "PersonalizationTags", "PreHeaderText", "RecipientType", "SenderEmail", "SenderName", "Status", "SubjectLine", "TemplateName", "UpdatedBy", "UpdatedTime" },
+                values: new object[,]
+                {
+                    { new Guid("760bc747-f5d3-40ea-ae00-aa0855e17aac"), "<p>Thank you for registering your Cursus account. Click here to go back the page</p>", "<a href=\"{{Login}}\">Login now</a>", "Verify", null, null, "<p>Contact us at cursusservicetts@gmail.com</p>", "English", "{FirstName}, {LinkLogin}", "User Account Verified!", "Customer", "cursusservicetts@gmail.com", "Cursus Team", 1, "Cursus Verify Email", "SendVerifyEmail", null, null },
+                    { new Guid("bc33ecfa-4ede-4a21-a2c0-4e3dca231996"), "Hi [UserFullName],<br><br>We received a request to reset your password. Click the link below to reset your password.", "https://cursuslms.xyz/sign-in/verify-email?userId=user.Id&token=Uri.EscapeDataString(token)", "Security", null, null, "If you did not request a password reset, please ignore this email.", "English", "[UserFullName], [ResetPasswordLink]", "Reset your password to regain access", "Customer", "cursusservicetts@gmail.com", "Cursus Team", 1, "Reset Your Password", "ForgotPasswordEmail", null, null },
+                    { new Guid("bec16645-d1fd-46d7-8903-90cffdfecfb5"), "Dear [UserFullName],<br><br>Welcome to Koi Veterinary Service Center! We are thrilled to have you as part of our community dedicated to the care and well-being of your beloved pets.", "<a href=\"{{VerificationLink}}\">Verify Your Email</a>", "Appointment", null, null, "<p>Contact us at koiveterinaryservice@gmail.com</p>", "English", "{FirstName}, {LastName}", "Thank you for signing up!", "Customer", "koiveterinaryservice@gmail.com", "Koi Veterinary Service Center", 1, "Welcome to Koi Veterinary Service Center!", "Appointment booked", null, null }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppointmentPets_PetId",
+                table: "AppointmentPets",
+                column: "PetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PetServices_ServiceId",
+                table: "PetServices",
+                column: "ServiceId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppointmentPets");
+
+            migrationBuilder.DropTable(
+                name: "PetServices");
+
+            migrationBuilder.DeleteData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: new Guid("760bc747-f5d3-40ea-ae00-aa0855e17aac"));
+
+            migrationBuilder.DeleteData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: new Guid("bc33ecfa-4ede-4a21-a2c0-4e3dca231996"));
+
+            migrationBuilder.DeleteData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: new Guid("bec16645-d1fd-46d7-8903-90cffdfecfb5"));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "BestZedAndYasuo",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "d785e24d-fb1b-4a15-aaf7-b49382d8701e", "AQAAAAIAAYagAAAAEMOMaALaDziK5I0XZ4GN6MEzWwmWkah0ZzpFYH8QXhs3JJWHoI2xuqFX+uniPyONLg==", "37f593a8-63e4-4653-8709-e834ca924a96" });
+
+            migrationBuilder.InsertData(
+                table: "EmailTemplates",
+                columns: new[] { "Id", "BodyContent", "CallToAction", "Category", "CreatedBy", "CreatedTime", "FooterContent", "Language", "PersonalizationTags", "PreHeaderText", "RecipientType", "SenderEmail", "SenderName", "Status", "SubjectLine", "TemplateName", "UpdatedBy", "UpdatedTime" },
+                values: new object[,]
+                {
+                    { new Guid("1c7b5b31-cf47-447b-aeea-bbed2d5ababa"), "<p>Thank you for registering your Cursus account. Click here to go back the page</p>", "<a href=\"{{Login}}\">Login now</a>", "Verify", null, null, "<p>Contact us at cursusservicetts@gmail.com</p>", "English", "{FirstName}, {LinkLogin}", "User Account Verified!", "Customer", "cursusservicetts@gmail.com", "Cursus Team", 1, "Cursus Verify Email", "SendVerifyEmail", null, null },
+                    { new Guid("30610ebb-7620-4ab7-b3af-a5642a67691d"), "Dear [UserFullName],<br><br>Welcome to Koi Veterinary Service Center! We are thrilled to have you as part of our community dedicated to the care and well-being of your beloved pets.", "<a href=\"{{VerificationLink}}\">Verify Your Email</a>", "Appointment", null, null, "<p>Contact us at koiveterinaryservice@gmail.com</p>", "English", "{FirstName}, {LastName}", "Thank you for signing up!", "Customer", "koiveterinaryservice@gmail.com", "Koi Veterinary Service Center", 1, "Welcome to Koi Veterinary Service Center!", "Appointment booked", null, null },
+                    { new Guid("488e6b78-b7bb-43e1-8678-b70e271ab2b5"), "Hi [UserFullName],<br><br>We received a request to reset your password. Click the link below to reset your password.", "https://cursuslms.xyz/sign-in/verify-email?userId=user.Id&token=Uri.EscapeDataString(token)", "Security", null, null, "If you did not request a password reset, please ignore this email.", "English", "[UserFullName], [ResetPasswordLink]", "Reset your password to regain access", "Customer", "cursusservicetts@gmail.com", "Cursus Team", 1, "Reset Your Password", "ForgotPasswordEmail", null, null }
+                });
+        }
+    }
+}
