0e8ef0eeed8d1d37b69a3c485b795fb03ea13160
khoidao45 <minhkhoidao18@gmail.com>
2024-10-30 15:28:48 +0700

    disea

---
+++ KoiVeterinaryServiceCenter_Server/KoiVeterinaryServiceCenter.Models/DTO/Disease/UpdateDiseaseDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KoiVeterinaryServiceCenter.Models.DTO
+{
+    public class UpdateDiseaseDTO
+    {
+        [Required]
+        public Guid DiseaseId { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Symptoms { get; set; }
+
+        public string? Medication { get; set; }
+    }
+}
